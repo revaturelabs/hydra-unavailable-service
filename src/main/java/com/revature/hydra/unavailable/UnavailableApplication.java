ff1b17b226859a93d9513d3f7cc206568753a538
Bryton Hall <email@bryton.io>
2018-04-06 21:55:20 -0400

    add swagger documentation

--- src/main/java/com/revature/hydra/unavailable/UnavailableApplication.java
+++ src/main/java/com/revature/hydra/unavailable/UnavailableApplication.java
@@ -2,8 +2,18 @@ package com.revature.hydra.unavailable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableDiscoveryClient
+@EnableSwagger2
+@Import(SpringDataRestConfiguration.class)
 public class UnavailableApplication {
 
 	public static void main(String[] args) {
